33356cad94b16872b538fcea3d06248b7a1b736c
servus033-cloud <servus033@gmail.com>
2025-11-27 13:35:08 +0100

    git add .

--- Serveur-Overtchat/obj/Debug/net9.0/Service-Overtchat.AssemblyInfo.fs
+++ Serveur-Overtchat/obj/Debug/net9.0/Service-Overtchat.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Service-Overtchat")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb82c6af11a1cb3cecd5ecfd6320c4aef7489cb")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e4b490f6bf6ba1047f5701860e78b3d65f0414")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Service-Overtchat")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Service-Overtchat")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
