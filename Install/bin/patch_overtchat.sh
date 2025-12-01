553f2d9de75090269a4fdf0ef3985bd0cf7d0d80
servus033-cloud <servus033@gmail.com>
2025-12-01 11:51:07 +0100

    git commit -a -m "update"

--- Install/bin/patch_overtchat.sh
+++ Install/bin/patch_overtchat.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# tools_local/patch_calls.sh
+# tools/patch_calls.sh
 set -euo pipefail
 IFS=$'\n\t'
 
--- Serveur-Overtchat/obj/Debug/net9.0/Service-Overtchat.AssemblyInfo.fs
+++ Serveur-Overtchat/obj/Debug/net9.0/Service-Overtchat.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Service-Overtchat")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59d8c60f5e8435aacd828879d2c622796b219086")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfee631ebb4e9007b214cca5be9cfc6470fee4c9")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Service-Overtchat")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Service-Overtchat")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
